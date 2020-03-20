40b7846f94880883a5d4666e0c93987d5c0815b0
KamikazeSloth <julius.emil.petersson@gmail.com>
2020-03-20 14:34:28 +0100

    setup navbar and links, beginning of vue router

---
+++ slothenterprise/src/components/NavigationBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="navBar">
+    <NavBarLink msg="link 1"/>
+    <NavBarLink msg="link 2"/>
+    <NavBarLink msg="link 3"/>
+  </nav>
+</template>
+
+<script>
+import NavBarLink from './NavBarLink.vue'
+
+export default {
+components: {
+    NavBarLink
+  },
+  name: 'NavigationBar',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .navBar {
+        display: flex;
+        flex-direction: row;
+    }
+</style>
