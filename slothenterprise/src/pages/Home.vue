1269db3f653c3dbe6104287fa1f734968a2094c5
KamikazeSloth <julius.emil.petersson@gmail.com>
2020-12-16 09:34:07 +0100

    auth0 implementering

--- slothenterprise/src/pages/Home.vue
+++ slothenterprise/src/pages/Home.vue
@@ -4,17 +4,36 @@ Vue.use(Plugin)
 <template>
   <fragment>
     <div>
+          <!-- Check that the SDK client is not currently loading before accessing is methods -->
+    <div v-if="!$auth.loading">
+      <!-- show login when not authenticated -->
+      <button v-if="!$auth.isAuthenticated" @click="login">Log in</button>
+      <!-- show logout when authenticated -->
+      <button v-if="$auth.isAuthenticated" @click="logout">Log out</button>
+    </div>
       this is home's content
     </div>
   </fragment>
 </template>
 
 <script>
+// .. imports removed for brevity
 
 export default {
-  name: 'Home'
-}
+  name: "home",
+  methods: {
+    // Log the user in
+    login() {
+      this.$auth.loginWithRedirect();
+    },
+    // Log the user out
+    logout() {
+      this.$auth.logout({
+        returnTo: window.location.origin
+      });
+    }
+  }
+};
 </script>
-
 <style>
 </style>
